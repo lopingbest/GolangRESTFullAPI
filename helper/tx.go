8065aa5e7299d09434b18d6cba3277f276b0a961
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-24 17:01:27 +0700

    feat(tx): add tx

--- helper/tx.go
+++ helper/tx.go
@@ -1,1 +1,16 @@
 package helper
+
+import "database/sql"
+
+func CommitOrRollback(tx *sql.Tx) {
+	//cek apakah eror atau tidak
+	err := recover()
+	if err != nil {
+		errorRollback := tx.Rollback()
+		PanicIfError(errorRollback)
+		panic(err)
+	} else {
+		errorCommit := tx.Commit()
+		PanicIfError(errorCommit)
+	}
+}
