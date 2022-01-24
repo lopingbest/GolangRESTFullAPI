c773d34c079a46ce3986f75a0df3e907d11ead6d
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-24 20:30:18 +0700

    feat(controller): update category_controller

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -7,9 +7,9 @@ import (
 
 type CategoryController interface {
 	//function mengikuti API yang akan dibuat. Parameter mengikuti http handler. Params diikutkan, menyesuaikan kontrak
-	Create(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	Update(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	Delete(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	FindById(w http.ResponseWriter, r http.Request, params httprouter.Params)
-	FIndAll(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	Create(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+	Update(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+	Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+	FindById(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+	FIndAll(w http.ResponseWriter, r *http.Request, params httprouter.Params)
 }
