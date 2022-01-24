3c248df82b6e9c87151fc1129bae7bcc80a8d880
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-24 21:53:53 +0700

    feat(httprouter): add main

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package GolangRESTFullAPI
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+	"lopingbest/GolangRESTFullAPI/app"
+	"lopingbest/GolangRESTFullAPI/controller"
+	"lopingbest/GolangRESTFullAPI/model/repository"
+	"lopingbest/GolangRESTFullAPI/model/service"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+	CategoryRepository := repository.NewCategoryRespositoryImplementation()
+	categoryservice := service.NewCategoryService(CategoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryservice)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+}
