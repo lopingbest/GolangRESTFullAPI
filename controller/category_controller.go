691bd2c922dfff792e43d7e345f6f998d93e98c4
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-24 18:05:02 +0700

    feat(restfull): add controller

---
+++ controller/category_controller.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+type CategoryController interface {
+	//function mengikuti API yang akan dibuat. Parameter mengikuti http handler. Params diikutkan, menyesuaikan kontrak
+	Create(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	Update(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	Delete(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	FindById(w http.ResponseWriter, r http.Request, params httprouter.Params)
+	FIndAll(w http.ResponseWriter, r http.Request, params httprouter.Params)
+}
